5d666cd198bae583ccfa9bdbda6363edbd41f18c
Kaizen <119792622+KaizenSheesh@users.noreply.github.com>
2024-02-26 16:59:41 +0700

    del

--- koneksi.php
+++
@@ -1,5 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost", "root", "", "kasirvanila");
-if(!$koneksi) {
-    echo "Koneksi Gagal";
-}
--- transaksi_function/keranjang_reset.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-
-$_SESSION['cart'] = [];
-header('location:../admin/transaksi.php');
-?>
